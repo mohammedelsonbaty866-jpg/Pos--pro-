e28db852dd64deb8313060109cd922178fc66f17
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-02 15:37:55 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,31 @@
 function login() {
-  if (username.value && password.value) {
-    location.href = "dashboard.html";
-  } else {
-    alert("ادخل البيانات");
-  }
+  const u = username.value;
+  const p = password.value;
+
+  const tx = db.transaction("users", "readonly");
+  const store = tx.objectStore("users");
+  const req = store.getAll();
+
+  req.onsuccess = () => {
+    const user = req.result.find(
+      x => x.username === u && x.password === p
+    );
+
+    if (!user) {
+      alert("بيانات غير صحيحة");
+      return;
+    }
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    if (user.role === "admin") {
+      location.href = "../dashboard.html";
+    } else {
+      location.href = "agents.html";
+    }
+  };
+}
+
+function currentUser() {
+  return JSON.parse(localStorage.getItem("user"));
 }
