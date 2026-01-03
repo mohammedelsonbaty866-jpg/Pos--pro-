812077fdd06828eb4c50c019e0a426d25d1d074e
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-03 02:51:56 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,8 @@ function toggleMenu() {
 function go(page) {
   window.location.href = page;
 }
+document.querySelectorAll(".menu-item").forEach(item => {
+  if (item.getAttribute("onclick")?.includes(location.pathname.split("/").pop())) {
+    item.classList.add("active");
+  }
+});
