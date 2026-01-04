810b179cd698b9c9c70d1ae72cf71de17a0c09b4
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-04 16:26:39 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,14 @@
-// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBZwWxWIIE0exAPoL9P8pbmp19gnBFxQq0",
   authDomain: "pos-pro-996f0.firebaseapp.com",
   projectId: "pos-pro-996f0",
-  storageBucket: "pos-pro-996f0.appspot.com",
+  storageBucket: "pos-pro-996f0.firebasestorage.app",
   messagingSenderId: "591451935128",
-  appId: "1:591451935128:web:683495139e62fb9b1e1bed",
+  appId: "1:591451935128:web:683495139e62fb9b1e1bed"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
