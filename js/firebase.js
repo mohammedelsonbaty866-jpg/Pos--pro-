0ad15bab6175634291d97be029c14e3f6861416b
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-05 00:42:56 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBZwWxWIIE0exAPoL9P8pbmp19gnBFxQq0",
   authDomain: "pos-pro-996f0.firebaseapp.com",
   projectId: "pos-pro-996f0",
-  storageBucket: "pos-pro-996f0.firebasestorage.app",
+  storageBucket: "pos-pro-996f0.appspot.com",
   messagingSenderId: "591451935128",
   appId: "1:591451935128:web:683495139e62fb9b1e1bed"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
