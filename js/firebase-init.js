69cd76dcdb13135fdaf0371c132ab729a9d29e0a
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-05 22:33:09 +0200

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,18 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT.firebaseapp.com",
+  projectId: "PROJECT",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
